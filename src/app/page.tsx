e07518fb164ce49ecb67c0db90a46489bee24518
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-10-12 16:42:57 +0100

    ""

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <nav className="w-[1472px] h-[84px] bg-[#F2F4F7] rounded-[100px] border-[1px] mt-[40px] border-[#D0D5DD]"></nav>
+      <Navbar />
     </main>
   );
 }
