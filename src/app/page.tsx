88f05ffc6779f397c4b2117a511a54383abcd576
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-10-13 12:50:17 +0100

    "

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Companies from "@/components/Companies";
 import Why from "@/components/Why";
+import Shopify from "@/components/Shopify";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-between">
@@ -17,6 +18,10 @@ export default function Home() {
 
       <div className="mt-[192px] ml-[160px]">
         <Why />
+      </div>
+
+      <div className="mt-[187px] ml-[160px]">
+        <Shopify />
       </div>
     </main>
   );
